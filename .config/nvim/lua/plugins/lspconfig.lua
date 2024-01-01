88dc8fd14a1ce1082ebb9416e0a0096b19c93688
bajelidze <irakli.bajelidze@exasol.com>
2024-01-01 23:13:20 +0100

    Setup new linters; Split the nvim setup

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,119 @@
+local function config()
+    local lspconfig = require('lspconfig')
+    local servers = {
+        'efm',
+        'clangd',
+        'tsserver',
+        'eslint',
+        'volar',
+        'lua_ls',
+        'gopls',
+        'golangci_lint_ls',
+        'pylsp',
+    }
+
+    -- Format on save
+    local function create_format_autocmd(bufnr)
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = bufnr,
+            command = 'lua vim.lsp.buf.format()',
+        })
+    end
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    for _, server in ipairs(servers) do
+        local opts = {
+            on_attach = function(_, bufnr)
+                create_format_autocmd(bufnr)
+            end,
+            capabilities = capabilities,
+        }
+
+        if server == 'efm' then
+            local shellcheck = {
+                lintCommand = 'shellcheck -f gcc -x -',
+                lintStdin = true,
+                lintFormats = {
+                    '%f:%l:%c: %trror: %m',
+                    '%f:%l:%c: %tarning: %m',
+                    '%f:%l:%c: %tote: %m',
+                },
+            }
+
+            local goimports = {
+                formatCommand = 'goimports',
+                formatStdin = true,
+            }
+
+            local languages = {
+                sh = { shellcheck },
+                go = { goimports },
+            }
+
+            opts.init_options = {
+                documentFormatting = true,
+                hover = false,
+                documentSymbol = false,
+                codeAction = false,
+                completion = false,
+            }
+            opts.settings = {
+                filetypes = vim.tbl_keys(languages),
+                rootMarkers = {
+                    'package.json',
+                    'pyproject.toml',
+                    'Cargo.toml',
+                    '.git/',
+                },
+                languages = languages,
+            }
+        elseif server == 'eslint' then
+            opts.on_attach = function(_, bufnr)
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    buffer = bufnr,
+                    command = 'EslintFixAll',
+                })
+            end
+        elseif server == 'volar' then
+            opts.filetypes = {
+                'typescript', 'javascript', 'javascriptreact',
+                'typescriptreact', 'vue', 'json',
+            }
+        elseif server == 'lua_ls' then
+            opts.on_init = function(client)
+                local path = client.workspace_folders[1].name
+                if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                    return true
+                end
+
+                client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                    Lua = {
+                        runtime = { version = 'LuaJIT' },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = { vim.env.VIMRUNTIME },
+                        },
+                    },
+                })
+
+                client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+                return true
+            end
+        elseif server == 'gopls' then
+            opts.settings = {
+                gopls = {
+                    gofumpt = true,
+                },
+            }
+        elseif server == 'golangci_lint_ls' then
+            opts.init_options = {
+                command = { 'golangci-lint', 'run', '--out-format', 'json' },
+            }
+        end
+
+        lspconfig[server].setup(opts)
+    end
+end
+
+return config
